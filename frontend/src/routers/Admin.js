3c73d16a26fa7fd9791e28dbf7c61c3a92dc8316
SangPT <phamtrongsang11@gmail.com>
2022-11-19 22:16:55 +0700

    update order

--- frontend/src/routers/Admin.js
+++ frontend/src/routers/Admin.js
@@ -1,45 +1,31 @@
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import DashBoard from "../screens/DashBoard";
-import AdminHome from "../components/AdminHome";
-import AdminOrder from "../components/AdminOrders";
-import AdminProducts from "../components/AdminProducts";
-import AdminProductForm from "../components/AdminProductForm";
-import AdminReceive from "../components/AdminReceive";
-import AdminReceiveForm from "../components/AdminReceiveForm";
-import AdminAccount from "../components/AdminAccount";
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import DashBoard from '../screens/DashBoard';
+import AdminHome from '../components/AdminHome';
+import AdminOrder from '../components/AdminOrders';
+import AdminProducts from '../components/AdminProducts';
+import AdminProductForm from '../components/AdminProductForm';
+import AdminReceive from '../components/AdminReceive';
+import AdminReceiveForm from '../components/AdminReceiveForm';
+import AdminAccount from '../components/AdminAccount';
 
 function Admin() {
-    return (
-        <BrowserRouter>
-            <DashBoard>
-                <Routes>
-                    <Route path="/admin/" element={<AdminHome />}></Route>
-                    <Route path="/admin/home" element={<AdminHome />}></Route>
-                    <Route
-                        path="/admin/orders"
-                        element={<AdminOrder />}></Route>
-                    <Route
-                        path="/admin/products"
-                        element={<AdminProducts />}></Route>
-                    <Route
-                        path="/admin/products/create"
-                        element={<AdminProductForm />}></Route>
-                    <Route
-                        path="/admin/products/update/:id"
-                        element="abc"></Route>
-                    <Route
-                        path="/admin/account"
-                        element={<AdminAccount />}></Route>
-                    <Route
-                        path="/admin/receive"
-                        element={<AdminReceive />}></Route>
-                    <Route
-                        path="/admin/receive/create"
-                        element={<AdminReceiveForm />}></Route>
-                </Routes>
-            </DashBoard>
-        </BrowserRouter>
-    );
+	return (
+		<BrowserRouter>
+			<DashBoard>
+				<Routes>
+					<Route path="/" element={<AdminHome />}></Route>
+					<Route path="/home" element={<AdminHome />}></Route>
+					<Route path="/orders" element={<AdminOrder />}></Route>
+					<Route path="/products" element={<AdminProducts />}></Route>
+					<Route path="/products/create" element={<AdminProductForm />}></Route>
+					<Route path="/products/update/:id" element="abc"></Route>
+					<Route path="/account" element={<AdminAccount />}></Route>
+					<Route path="/receive" element={<AdminReceive />}></Route>
+					<Route path="/receive/create" element={<AdminReceiveForm />}></Route>
+				</Routes>
+			</DashBoard>
+		</BrowserRouter>
+	);
 }
 
 export default Admin;
--- frontend/src/screens/DashBoard.js
+++ frontend/src/screens/DashBoard.js
@@ -1,88 +1,87 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Row, Col, Container } from "react-bootstrap";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Container } from 'react-bootstrap';
 
 const DashBoard = ({ children }) => {
-    const [content, setContent] = useState("home");
+	const [content, setContent] = useState('home');
 
-    return (
-        <Container fluid={true} className="dashboard">
-            <Row className="g-0">
-                <Col xs={2}>
-                    <div className="navigation">
-                        <ul>
-                            <li>
-                                <i className="fa-light fa-house"></i>
-                                <div>Dashboard</div>
-                            </li>
-                            <li
-                                className={content === "home" ? "active" : ""}
-                                onClick={() => setContent("home")}>
-                                <Link to="/admin/home">
-                                    <i className="fa-light fa-house"></i>
-                                    <div>Home</div>
-                                </Link>
-                            </li>
-                            <li
-                                className={content === "orders" ? "active" : ""}
-                                onClick={() => setContent("orders")}>
-                                <Link to="/admin/orders">
-                                    <i className="fa-light fa-house"></i>
-                                    <div>Orders</div>
-                                </Link>
-                            </li>
-                            <li
-                                className={
-                                    content === "products" ? "active" : ""
-                                }
-                                onClick={() => setContent("products")}>
-                                <Link to="/admin/products">
-                                    <i className="fa-light fa-house"></i>
-                                    <div>Product</div>
-                                </Link>
-                            </li>
-                            <li
-                                className={
-                                    content === "account" ? "active" : ""
-                                }
-                                onClick={() => setContent("account")}>
-                                <Link to="/admin/account">
-                                    <i className="fa-light fa-house"></i>
-                                    <div>Account</div>
-                                </Link>
-                            </li>
-                            <li
-                                className={
-                                    content === "receive" ? "active" : ""
-                                }
-                                onClick={() => setContent("receive")}>
-                                <Link to="/admin/receive">
-                                    <i className="fa-light fa-house"></i>
-                                    <div>Receive</div>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </Col>
-                <Col xs={10}>
-                    <div className="content">
-                        <div className="admin-header">
-                            <div className="icon">
-                                <i className="fa-regular fa-bars"></i>
-                            </div>
-                            <div className="avatar">
-                                <div className="img">
-                                    <img src="/images/p5.jpg" alt="" />
-                                </div>
-                                <div className="name">Tèo</div>
-                            </div>
-                        </div>
-                        {children}
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    );
+	return (
+		<Container fluid={true} className="dashboard">
+			<Row className="g-0">
+				<Col xs={2}>
+					<div className="navigation">
+						<ul>
+							<li>
+								<i className="fa-light fa-house"></i>
+								<div>Dashboard</div>
+							</li>
+							<li
+								className={content === 'home' ? 'active' : ''}
+								onClick={() => setContent('home')}
+							>
+								<Link to="/home">
+									<i className="fa-light fa-house"></i>
+									<div>Home</div>
+								</Link>
+							</li>
+							<li
+								className={content === 'orders' ? 'active' : ''}
+								onClick={() => setContent('orders')}
+							>
+								<Link to="/orders">
+									<i className="fa-light fa-house"></i>
+									<div>Orders</div>
+								</Link>
+							</li>
+							<li
+								className={content === 'products' ? 'active' : ''}
+								onClick={() => setContent('products')}
+							>
+								<Link to="/products">
+									<i className="fa-light fa-house"></i>
+									<div>Product</div>
+								</Link>
+							</li>
+							<li
+								className={content === 'account' ? 'active' : ''}
+								onClick={() => setContent('account')}
+							>
+								<Link to="/account">
+									<i className="fa-light fa-house"></i>
+									<div>Account</div>
+								</Link>
+							</li>
+							<li
+								className={content === 'receive' ? 'active' : ''}
+								onClick={() => setContent('receive')}
+							>
+								<Link to="/receive">
+									<i className="fa-light fa-house"></i>
+									<div>Receive</div>
+								</Link>
+							</li>
+						</ul>
+					</div>
+				</Col>
+				<Col xs={10}>
+					<div className="content">
+						<div className="admin-header">
+							<div className="icon">
+								<i className="fa-regular fa-bars"></i>
+							</div>
+							<div className="avatar">
+								<div className="img">
+									<img src="/images/p5.jpg" alt="" />
+								</div>
+								<div className="name">Tèo</div>
+							</div>
+						</div>
+						{children}
+					</div>
+				</Col>
+			</Row>
+		</Container>
+	);
 };
 
 export default DashBoard;
